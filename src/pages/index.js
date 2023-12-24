e402f1600b95dec48147351f7b024b688f49f98d
KDM121 <krnmcdnnll@gmail.com>
2023-12-24 00:40:54 +0000

    main services templated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,232 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <main>
-        <div className="flex justify-center w-screen bg-base-200">
+      <main className="bg-base-100">
+        <div className="flex justify-center w-screen bg-base-100">
           <h1 className="text-5xl font-bold text text-center py-5 border-y-2">
             The Dashboard of Services
           </h1>
+        </div>
+        {/*main services*/}
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
+          {/*Service 1*/}
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.proxmox.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://192.168.0.10:8006"
+                >
+                  Proxmox
+                </a>
+              </div>
+            </div>
+          </div>
+          {/*Service 2*/}
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.kasmweb.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://kasm.kieranmcdonnell.com"
+                >
+                  Kasm
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://github.com/everywall/ladder/raw/main/assets/pigeon.svg"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://ladder.kieranmcdonnell.com"
+                >
+                  Ladder
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.kieranmcdonnell.com/favicon.ico"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://www.kieranmcdonnell.com/"
+                >
+                  Website
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.openmediavault.org/apple-touch-icon.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="http://192.168.0.20:80"
+                >
+                  OpenMediaVault
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://github.com/alexta69/metube/blob/master/favicon/android-chrome-384x384.png?raw=true"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://metube.kieranmcdonnell.com"
+                >
+                  Metube
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.proxmox.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://192.168.0.10:8006"
+                >
+                  Proxmox
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.proxmox.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://192.168.0.10:8006"
+                >
+                  Proxmox
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.proxmox.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://192.168.0.10:8006"
+                >
+                  Proxmox
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.proxmox.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://192.168.0.10:8006"
+                >
+                  Proxmox
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.proxmox.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://192.168.0.10:8006"
+                >
+                  Proxmox
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card w-full md:w-96 bg-base-100 shadow-xl image-full mx-6">
+            <figure className="w-64 h-48 overflow-hidden">
+              <img
+                src="https://www.proxmox.com/android-chrome-512x512.png"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex flex-col">
+              <div className="card-actions justify-end">
+                <a
+                  className="btn btn-primary btn-lg"
+                  href="https://192.168.0.10:8006"
+                >
+                  Proxmox
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1e40af",
+          secondary: "#786141",
+          accent: "#157870",
+          neutral: "#2a323c",
+          "base-100": "#1f2937",
+        },
+      },
+    ],
+  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
